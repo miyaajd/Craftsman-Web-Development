4744a9b376ba828beba6459bb86cb3a296ffecae
miyaajd <ehdal1032@gmail.com>
2025-09-05 18:11:17 +0900

    js

--- js/a-2.js
+++ js/a-2.js
@@ -5,4 +5,13 @@ $(function(){
     $("#first").click(function(){
         $(".txt-modal").show()
     })
+    
+    const slideWidth = $(".slide img").width()
+    const moveSlide = ()=>{
+        slide.animate({marginLeft: -slideWidth},1000,function(){
+            $(this).append($(this).find(".slide img:first"))
+            $(this).css({marginLeft : 0})
+        })
+    }
+    setInterval(moveSlide, 3000)
 })
