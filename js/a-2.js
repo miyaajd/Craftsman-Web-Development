c12cc5606d16f4517c3cb3d164415384e7a2864c
miyaajd <ehdal1032@gmail.com>
2025-09-08 10:34:29 +0900

    green done

--- js/a-2.js
+++ js/a-2.js
@@ -1,17 +1,28 @@
-$(function(){
-    $("#close").click(function(){
-        $(".txt-modal").hide()
-    })
-    $("#first").click(function(){
-        $(".txt-modal").show()
-    })
-    
-    const slideWidth = $(".slide img").width()
-    const moveSlide = ()=>{
-        slide.animate({marginLeft: -slideWidth},1000,function(){
-            $(this).append($(this).find(".slide img:first"))
-            $(this).css({marginLeft : 0})
-        })
-    }
-    setInterval(moveSlide, 3000)
-})
+$(function () {
+  $("#first").click(function () {
+    $(".txt-modal").show();
+  });
+  $("#gallery-first").click(function () {
+    $(".gallery-modal").show();
+  });
+  $("#close").click(function () {
+    $(".modal").hide();
+  });
+  $(".gallery-close").click(function(){
+    $(".modal").hide()
+  })
+  const slideList = $(".img-slide");
+  const slideWidth = $(".img-slide li ").width();
+  const moveSlide = () => {
+    slideList.animate({ marginLeft: -slideWidth }, 1000, function () {
+      $(this).append($(this).find("li:first-child"));
+      $(this).css({ marginLeft: 0 });
+    });
+  };
+  setInterval(moveSlide, 3000);
+// 
+$(".nav > li ").hover(function(){
+    $(".depth2Bg, .depth2").stop().slideDown(300)}
+,function(){
+    $(".depth2Bg, .depth2").stop().slideUp(300)})
+});
