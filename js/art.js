e3567ac2895adfec22a476b0acc4b07e97671d2c
miyaajd <ehdal1032@gmail.com>
2025-09-09 14:17:54 +0900

    art done

---
+++ js/art.js
@@ -0,0 +1,18 @@
+$(function () {
+  const slide = $(".slide-list");
+  const slideWidth = $(".slide-list li").width();
+  const moveSlide = () => {
+    slide.animate({ marginLeft: -slideWidth }, 1000, function () {
+      $(this).append($(this).find("li:first-child"));
+      $(this).css({ marginLeft: 0 });
+    });
+  };
+  setInterval(moveSlide, 3000);
+  //   modal
+  $("#first").click(function () {
+    $(".modal").fadeIn(300);
+  });
+  $("#close").click(function(){
+    $(".modal").fadeOut(300)
+  })
+});
