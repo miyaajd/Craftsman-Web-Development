2cdbdac8158abc5ae73eea951c8a626620477c5e
miyaajd <ehdal1032@gmail.com>
2025-09-15 17:52:17 +0900

    train done

---
+++ js/train.js
@@ -0,0 +1,29 @@
+$(function () {
+  // modal
+  $("#first").click(function (e) {
+    e.preventDefault();
+    $(".modal").fadeIn(300);
+  });
+  $("#close").click(function (e) {
+    e.preventDefault();
+    $(".modal").fadeOut(300);
+  });
+
+  //   nav
+  $(".nav li").mouseenter(function () {
+    $(this).find(".depth2").stop().fadeIn(300);
+  });
+  $(".nav li").mouseleave(function () {
+    $(this).find(".depth2").stop().fadeOut(300);
+  });
+
+  // slide
+  const slide = $(".slide-list");
+  const slideWidth = $(".slide-list li").width();
+  const moveSlide = () => {
+    slide.animate({ marginLeft: -slideWidth }, 1000, function () {
+      $(this).append($(this).find("li:first-child"));
+      $(this).css({ marginLeft: 0 });
+    });
+  };setInterval(moveSlide,3000)
+});
